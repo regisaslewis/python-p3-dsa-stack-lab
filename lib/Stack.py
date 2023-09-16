8494ee518b6c88f818c7d657e6090009a773816d
RegisLewis <regisaslewis@gmail.com>
2023-09-15 18:30:48 -0700

    Stack Lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,49 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = []
+        self.limit = limit
+
+        for n in items:
+            if self.full() == False:
+                self.items.append(n)
 
     def isEmpty(self):
-        pass
+        return self.items == []
 
     def push(self, item):
-        pass
+        if self.full():
+            return None
+        self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
+        return self.items[- 1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return (self.size() - self.items.index(target)) - 1
+        return -1
+
+# test = Stack([3, 4], 3)
+# print(test.full())
+# test.push(0)
+# print(test.full())
+# print(test.items)
+# test.push(3)
+# print(test.full())
+# print(test.items)
+# print(test.peek())
+# print(test.search(3))
+# print(test.search(0))
+# print(test.search(5))
